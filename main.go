09e24c05219494c3732d838aa543a7f978dd9ada
Joop Kiefte <joop@kiefte.net>
2016-04-30 23:36:50 +0000

    Add first commands, correct for supergroups (int64)

--- main.go
+++ main.go
@@ -8,40 +8,42 @@ import (
 	"google.golang.org/appengine/urlfetch"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
 const URL = "https://api.telegram.org/bot" + TOKEN + "/"
 
 type Incoming struct {
-	UpdateID int32 `json:"update_id"`
+	UpdateID int64 `json:"update_id"`
 	Message  struct {
-		MessageID int32 `json:"message_id"`
+		MessageID int64 `json:"message_id"`
 		From      struct {
-			ID        int32
+			ID        int64
 			FirstName string `json:"first_name"`
 			LastName  string `json:"last_name"`
 			Username  string
 			Type      string
 		}
 		Chat struct {
-			ID        int32
+			ID        int64
 			FirstName string `json:"first_name"`
 			LastName  string `json:"last_name"`
 			Username  string
 			Type      string
 		}
-		Date     int32
+		Date     int64
 		Text     string
 		Entities []struct {
 			Type   string
-			Offset int32
-			Length int32
+			Offset int64
+			Length int64
 		}
 	}
 }
 
 func telegram(w http.ResponseWriter, r *http.Request) {
+	var mymessage string
 	c := appengine.NewContext(r)
 	client := &http.Client{
 		Transport: &urlfetch.Transport{
@@ -59,7 +61,16 @@ func telegram(w http.ResponseWriter, r *http.Request) {
 		log.Errorf(c, "%v", err)
 	}
 	log.Debugf(c, "%v", Output)
-	client.Post(URL+"sendMessage", "application/json", strings.NewReader(fmt.Sprintf("{\"chat_id\": %v, \"text\": \"Saluton %v!\n%v\"}", Output.Message.Chat.ID, Output.Message.From.FirstName, Output.Message.Text)))
+	command := regexp.MustCompile("/[^ ]*").FindString(Output.Message.Text)
+	switch command {
+	case "/start", "/start@pendumulobot":
+		mymessage = "Jes! Mi funkcias!"
+	case "/echo":
+		mymessage = regexp.MustCompile(`(["\\])`).ReplaceAllString(Output.Message.Text[5:], `\$1`)
+	case "/maldeculo":
+		mymessage = "Kiu? Mi? Eble vi estas, " + Output.Message.From.FirstName + "... \U0001F60F"
+	}
+	client.Post(URL+"sendMessage", "application/json", strings.NewReader(fmt.Sprintf("{\"chat_id\": %v, \"text\": \"%v\"}", Output.Message.Chat.ID, mymessage)))
 }
 
 func init() {
