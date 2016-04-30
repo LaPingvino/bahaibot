2e8012156207c5bbebe2993cb935c0417bcff2ba
Joop Kiefte <joop@kiefte.net>
2016-04-30 19:05:45 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+}
+
+func init() {
+	http.HandleFunc("/", handler)
+}
